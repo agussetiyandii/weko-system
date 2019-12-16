81f550dc2a3a180095d8ffd7a59b4420ca9f2dcb
fadhilc97 <fadhilc97@gmail.com>
2019-12-17 00:27:58 +0700

    modifikasi controller

--- application/controllers/Produk.php
+++ application/controllers/Produk.php
@@ -10,6 +10,25 @@ class Produk extends CI_Controller {
         $this->load->view('tambah-produk');
     }
 
+    public function simpan_produk(){
+        $kategori = $this->input->post('kategori');
+        $sub_kategori = $this->input->post('sub_kategori');
+        $lokasi = $this->input->post('lokasi');
+        $sub_lokasi = $this->input->post('sub_lokasi');
+
+        $data = [
+            'asal_barang' => $this->input->post('asalBarang'),
+            'nama_barang' => $this->input->post('namaBarang'),
+            'merek' => $this->input->post('merek'),
+            
+            'kondisi' => $this->input->post('kondisi'),
+            'harga' => $this->input->post('harga'),
+            'jumlah' => $this->input->post('jumlah'),
+            'remark' => $this->input->post('remark')
+        ];
+        $this->db->insert('t_produk');
+    }
+
 }
 
 ?>
