cc55436d6a365589544a36f161c5196adc74f738
fadhilc97 <fadhilc97@gmail.com>
2019-12-19 14:31:25 +0700

    perubahan struktur t_sub_lokasi: terdapat sub_lokasi_1 dan sub_lokasi_2

--- sql/weko_system.sql
+++ sql/weko_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 16, 2019 at 06:26 PM
+-- Generation Time: Dec 19, 2019 at 08:30 AM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.7
 
@@ -151,6 +151,72 @@ CREATE TABLE `t_sub_kategori` (
   `sub_kategori` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `t_sub_kategori`
+--
+
+INSERT INTO `t_sub_kategori` (`id`, `id_kategori`, `sub_kategori`) VALUES
+(1, 1, 'NOTEBOOK'),
+(2, 1, 'GAMING'),
+(3, 1, 'APPLE'),
+(4, 1, 'TABLET'),
+(5, 1, 'WORKSTATION'),
+(6, 2, 'MOUSE'),
+(7, 2, 'BATERAI'),
+(8, 2, 'COOLER FAN'),
+(9, 2, 'FLASHDISK'),
+(10, 2, 'KABEL POWER'),
+(11, 2, 'ADAPTOR'),
+(12, 2, 'KABEL DVI'),
+(13, 2, 'KEYBOARD'),
+(14, 2, 'KABEL VGA'),
+(15, 2, 'OPTICAL DRIVE'),
+(16, 2, 'PORTABLE HDD'),
+(17, 2, 'TAS DAN CASE'),
+(18, 2, 'WEB CAM'),
+(19, 2, 'SPEAKER'),
+(20, 2, 'AKSESORIS LAINNYA'),
+(21, 3, 'ALL IN ONE'),
+(22, 3, 'CPU'),
+(23, 3, 'MINI PC'),
+(24, 3, 'TOWER'),
+(25, 3, 'SFF'),
+(26, 4, 'LCD PC'),
+(27, 4, 'LED PC'),
+(28, 4, 'LCD LAPTOP'),
+(29, 4, 'LED LAPTOP'),
+(30, 4, 'LCD TOUCHSCREENS'),
+(31, 5, 'TOWER'),
+(32, 5, 'UDP'),
+(33, 5, 'CISCO'),
+(34, 6, 'AKSESORIS PRINTER'),
+(35, 6, 'CARTRIDGE'),
+(36, 6, 'COPIER'),
+(37, 6, 'TINTA PRINTER'),
+(38, 6, 'UNIT PRINTER'),
+(39, 7, 'AKSESORIS HP'),
+(40, 7, 'MEMORY CARD'),
+(41, 7, 'POWER BANK'),
+(42, 7, 'SMARTPHONE'),
+(43, 7, 'SMARTWATCH'),
+(44, 7, 'SPARE PARTS HP'),
+(45, 7, 'TABLET'),
+(46, 8, 'RAM'),
+(47, 8, 'HARDDISK'),
+(48, 8, 'SSD'),
+(49, 8, 'MSATA'),
+(50, 8, 'VGA'),
+(51, 8, 'PROCESSOR'),
+(52, 8, 'MOTHERBOARD'),
+(53, 8, 'UPS'),
+(54, 9, 'AC'),
+(55, 9, 'CD DVC BLU RAY'),
+(56, 9, 'HEADPHONE'),
+(57, 9, 'HOME THEATER'),
+(58, 9, 'PROYEKTOR'),
+(59, 9, 'TELEPON'),
+(60, 9, 'TV');
+
 -- --------------------------------------------------------
 
 --
@@ -160,8 +226,32 @@ CREATE TABLE `t_sub_kategori` (
 CREATE TABLE `t_sub_lokasi` (
   `id` int(11) NOT NULL,
   `id_lokasi` int(11) NOT NULL,
-  `sub_lokasi` varchar(128) NOT NULL
+  `sub_lokasi_1` varchar(128) NOT NULL,
+  `sub_lokasi_2` varchar(128) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `t_sub_lokasi`
+--
+
+INSERT INTO `t_sub_lokasi` (`id`, `id_lokasi`, `sub_lokasi_1`, `sub_lokasi_2`) VALUES
+(1, 1, 'LT.1 (Gudang)', NULL),
+(2, 1, 'LT.2', NULL),
+(3, 2, 'LT.1', 'RAK 1'),
+(4, 2, 'LT.1', 'RAK 2'),
+(5, 2, 'LT.1', 'RAK 3'),
+(6, 2, 'LT.1', 'RAK 4'),
+(7, 2, 'LT.2', 'RAK 1'),
+(8, 2, 'LT.2', 'RAK 2'),
+(9, 2, 'LT.2', 'RAK 3'),
+(10, 2, 'LT.2', 'RAK 4'),
+(11, 2, 'LT.3', 'RAK 1'),
+(12, 2, 'LT.3', 'RAK 2'),
+(13, 2, 'LT.3', 'RAK 3'),
+(14, 2, 'LT.3', 'RAK 4'),
+(15, 3, 'LT.1', NULL),
+(16, 3, 'LT.2', NULL),
+(17, 4, 'GUDANG', NULL);
 
 -- --------------------------------------------------------
 
@@ -268,13 +358,13 @@ ALTER TABLE `t_stok_barang`
 -- AUTO_INCREMENT for table `t_sub_kategori`
 --
 ALTER TABLE `t_sub_kategori`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT for table `t_sub_lokasi`
 --
 ALTER TABLE `t_sub_lokasi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `t_user`
